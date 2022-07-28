6841b534b2f0e28e37c9b8cec8c51f5e4761bb02
mateuszparzys123@gmail.com <mateuszparzys123@gmail.com>
2022-07-28 18:49:47 +0200

    refactored project structure

--- user-service/src/main/java/com/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/userservice/UserServiceApplication.java
@@ -1,7 +1,5 @@
 package com.userservice;
 
-import com.userservice.model.Employee;
-import com.userservice.model.EmployeeLevel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,13 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		CrudService cs = new CrudService();
-		Employee user = new Employee();
-		user.setEmployeeId(123);
-		user.setEmployeeLevel(EmployeeLevel.EMPLOYEE);
-		user.setEmployeeLogin("123");
-		user.setEmployeePassword("123");
-		user.setSeat("12");
 		SpringApplication.run(UserServiceApplication.class, args);
 	}
 
